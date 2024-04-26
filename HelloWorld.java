4591d1161e71f1c46eb3c91d7ec0004e3928005a
typicaleoxx <lsneha991@gmail.com>
2024-04-26 10:56:06 +0545

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+ 1. Function or methods - used to perform certain task
+ void main(){
+
+ } - we used this function to print hello world, since it doesnot return anything we put void
+
+ 2. Class - big entity where all the used funtions are included 
+ class Main{ 
+    void main(){ - this is the first function that will be inially run
+
+    }
+ }
+ */
