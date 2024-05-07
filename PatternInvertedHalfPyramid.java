b675f2e67b1596c5f119fdcf8076f9d19b0737ab
typicaleoxx <lsneha991@gmail.com>
2024-05-07 14:30:43 +0545

    added 4 pattern solutions

---
+++ PatternInvertedHalfPyramid.java
@@ -0,0 +1,11 @@
+public class PatternInvertedHalfPyramid {
+    public static void main(String[] args) {
+        int n = 1;
+        for (int i = 4; i >= n; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
